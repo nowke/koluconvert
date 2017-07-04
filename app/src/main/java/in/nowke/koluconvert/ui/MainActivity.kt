ed95ffa09bcdaca62bdb13db1cd7182e8712f3b7
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2017-07-04 11:54:47 +0530

    Add Color to ViewPager on page change

--- app/src/main/java/in/nowke/koluconvert/ui/MainActivity.kt
+++ app/src/main/java/in/nowke/koluconvert/ui/MainActivity.kt
@@ -3,23 +3,46 @@ package `in`.nowke.koluconvert.ui
 import `in`.nowke.koluconvert.R
 import `in`.nowke.koluconvert.ui.common.UnitTabAdapter
 
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var mSectionsPagerAdapter: UnitTabAdapter? = null
+    private var adapter: UnitTabAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        setupUI()
+    }
 
-        mSectionsPagerAdapter = UnitTabAdapter(supportFragmentManager)
-        container.adapter = mSectionsPagerAdapter
+    private fun setupUI() {
+        adapter = UnitTabAdapter(supportFragmentManager)
+        container.adapter = adapter
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+
+        // ViewPager onChange
+        container.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {}
+
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                val (color, colorDark) = adapter!!.getTabColor(position, positionOffset, this@MainActivity)
+                tabs.setBackgroundColor(color)
+                toolbar.setBackgroundColor(color)
+                appbar.setBackgroundColor(color)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    window.navigationBarColor = color
+                    window.statusBarColor = colorDark
+                }
+            }
+        })
     }
 }
--- app/src/main/java/in/nowke/koluconvert/ui/common/UnitTabAdapter.kt
+++ app/src/main/java/in/nowke/koluconvert/ui/common/UnitTabAdapter.kt
@@ -1,17 +1,23 @@
 package `in`.nowke.koluconvert.ui.common
 
+import `in`.nowke.koluconvert.R
 import `in`.nowke.koluconvert.ui.foot.FootFragment
 import `in`.nowke.koluconvert.ui.kolu.KoluFragment
 import `in`.nowke.koluconvert.ui.meter.MeterFragment
 
+import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.ColorUtils
 
 /**
  * UnitTabAdapter
  */
 class UnitTabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    data class ColorSet(val color: Int, val colorDark: Int)
 
     override fun getItem(position: Int): Fragment? {
         when (position) {
@@ -24,5 +30,32 @@ class UnitTabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getCount(): Int {
         return 3
+    }
+
+    fun getTabColor(position: Int, positionOffset: Float, context: Context): ColorSet {
+        val color1: Int = ContextCompat.getColor(context, R.color.colorPrimary)
+        val color1Dark: Int = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+        val color2: Int = ContextCompat.getColor(context, R.color.colorOrangePrimary)
+        val color2Dark: Int = ContextCompat.getColor(context, R.color.colorOrangePrimaryDark)
+        val color3: Int = ContextCompat.getColor(context, R.color.colorBluePrimary)
+        val color3Dark: Int = ContextCompat.getColor(context, R.color.colorBluePrimaryDark)
+
+        when (position) {
+            0 -> {
+                return ColorSet(
+                        ColorUtils.blendARGB(color1, color2, positionOffset),
+                        ColorUtils.blendARGB(color1Dark, color2Dark, positionOffset)
+                )
+            }
+            1 -> {
+                return ColorSet(
+                        ColorUtils.blendARGB(color2, color3, positionOffset),
+                        ColorUtils.blendARGB(color2Dark, color3Dark, positionOffset)
+                )
+            }
+            else -> {
+                return ColorSet(color3, color3Dark)
+            }
+        }
     }
 }
